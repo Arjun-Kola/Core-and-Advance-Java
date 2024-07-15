1ecbc2a98e145f532cab4306de7c447674c84a3f
Arjun <arjunsrikark@gmail.com>
2024-07-15 17:46:45 +0530

    firstcommit

---
+++ Advanced_Java/src/main/java/Advanced/Java/App.java
@@ -0,0 +1,13 @@
+package Advanced.Java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
